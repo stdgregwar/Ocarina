c08d94c14d54035407ba92b10365ab69cc07fbff
jmlind <beside.the.fire@gmail.com>
2011-11-01 19:42:25 -0300

    rolled back. need to make sure I have the abstraction layers correct.

--- src/com/instruments/ocarina/ui/OcarinaUI.java
+++ src/com/instruments/ocarina/ui/OcarinaUI.java
@@ -4,17 +4,5 @@ import android.app.Activity;
 
 public abstract class OcarinaUI extends Activity {
 	
-	@Override
-	protected void onCreate(Bundle savedInstanceState);
-	{
-		super.onCreate(savedInstanceState);
-		
-		initialize();
-	}
-	
-	
-	private void initialize()
-	{
-		// initialize UI elements
-	}
+
 }
