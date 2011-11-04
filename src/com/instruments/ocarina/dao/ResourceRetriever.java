c91cea194008b6f3c53528f7a3595da56db4baf9
Kevin Whitaker <eyecreate@gmail.com>
2011-11-03 21:49:51 -0400

    Added methods to ResourceRetriever so it can handle local files. Added other base structure to make class usable.

--- src/com/instruments/ocarina/dao/ResourceRetriever.java
+++ src/com/instruments/ocarina/dao/ResourceRetriever.java
@@ -1,7 +1,76 @@
 package com.instruments.ocarina.dao;
 
+import java.io.*;
+
 import android.net.Uri;
 
 public class ResourceRetriever {
-          private Uri resourceDirectory;
+	private Uri cacheDirectory; //local location to cache files.
+
+	/**
+	 * Constructor for class.
+	 * 
+	 * @param cacheDir
+	 *            File object that should contain the directory to use for
+	 *            cache. Usually gotten with getCacheDir() of app context.
+	 */
+	public ResourceRetriever(File cacheDir) {
+		cacheDirectory = Uri.parse("file://" + cacheDir.getAbsolutePath());
+	}
+
+	/**
+	 * Method to get resource from multiple possible locations and cache it
+	 * locally.
+	 * 
+	 * @param resource
+	 *            Uri that describes the resource and location to to cached.
+	 * @return File object containing the path to the cached file.
+	 */
+	public File getResource(Uri resource) {
+		File cachedResource = null;
+		if (resource.getScheme() == "file") {
+			if ((new File(cacheDirectory.getPath() + File.pathSeparator
+					+ (new File(resource.getPath())).getName())).exists()) {
+				cachedResource = new File(cacheDirectory.getPath()
+						+ File.pathSeparator
+						+ (new File(resource.getPath())).getName());
+			} else {
+				cachedResource = new File(cacheDirectory.getPath()
+						+ File.pathSeparator
+						+ (new File(resource.getPath())).getName());
+				try {
+					copyFile(new FileInputStream(new File(resource.getPath())),
+							new FileOutputStream(cachedResource));
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		// TODO:Add support for other Schemes like http.
+		return cachedResource;
+	}
+
+	/**
+	 * Method to byte copy one stream to another (particularly to be used for
+	 * files).
+	 * 
+	 * @param in
+	 *            InputStream object containing the input file data.
+	 * @param out
+	 *            OutputStream object to write file data to.
+	 * @throws IOException
+	 *             Exception to throw when unable to read/write data.
+	 */
+	private void copyFile(InputStream in, OutputStream out) throws IOException {
+		byte[] buffer = new byte[1024];
+		int read;
+		while ((read = in.read(buffer)) != -1) {
+			out.write(buffer, 0, read);
+		}
+	}
+
 }
