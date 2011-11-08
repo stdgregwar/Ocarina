4b720555324c8e64fdf7fa5b167b595090d55ee5
Kevin Whitaker <eyecreate@gmail.com>
2011-11-08 13:35:58 -0500

    Added base constructor for soundplayer

--- src/com/instruments/ocarina/service/SoundPlayer.java
+++ src/com/instruments/ocarina/service/SoundPlayer.java
@@ -1,7 +1,17 @@
 package com.instruments.ocarina.service;
 
+import java.io.File;
+
+import com.instruments.ocarina.dao.ResourceRetriever;
+
 import android.media.SoundPool;
 
 public class SoundPlayer {
         private SoundPool audioPlayer;
+        private ResourceRetriever resourceGet;
+        
+        public SoundPlayer(File cacheDir)
+        {
+        	//
+        }
 }
