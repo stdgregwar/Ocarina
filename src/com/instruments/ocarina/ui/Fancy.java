0e4435aad8a4747710de41de7996469c468b47dc
Kevin Whitaker <eyecreate@gmail.com>
2011-11-19 15:58:11 -0500

    This time for real

--- src/com/instruments/ocarina/ui/Fancy.java
+++ src/com/instruments/ocarina/ui/Fancy.java
@@ -5,8 +5,7 @@ import java.util.ArrayList;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TableRow.LayoutParams;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
@@ -25,35 +24,35 @@ public class Fancy extends OcarinaUI {
 	{
 		super.onCreate(savedInstanceState);
 		
-		setContentView((View)createLayout());
-//		OcarinaButton ocarinaButton = new OcarinaButton(Keys.FIVE, getSoundPlayer(), this);
-//		ocarinaButton.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-//		setContentView(ocarinaButton);
+//		setContentView(createLayout());
+		OcarinaButton ocarinaButton = new OcarinaButton(Keys.FIVE, getSoundPlayer(), this);
+		ocarinaButton.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+		setContentView(ocarinaButton);
 	}
 
-	private LinearLayout createLayout() {
-		ArrayList<OcarinaButton> ocarinaButtons = new ArrayList<OcarinaButton>();
-		// iterate through all the buttons to set their layout parameters
-		for (int i = 0; i < Keys.values().length; i++)
-		{
-			OcarinaButton ocarinaButton = new OcarinaButton(Keys.values()[i], getSoundPlayer(), this);
-			ocarinaButton.setLayoutParams(new LayoutParams(100, 100));
-			ocarinaButtons.add(ocarinaButton);
-		}
-		
-		// create the layout
-		LinearLayout tableLayout = new LinearLayout(this);
-		tableLayout.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-		
-		// add ocarina buttons to a row and add the row to the parent table
-		for (OcarinaButton ocarinaButton : ocarinaButtons)
-		{
-			//TableRow tableRow = new TableRow(this);
-			//tableRow.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-			//tableRow.addView((View)ocarinaButton);
-			tableLayout.addView((View)ocarinaButton);
-		}
-		
-		return tableLayout;
-	}	
+//	private TableLayout createLayout() {
+//		ArrayList<OcarinaButton> ocarinaButtons = new ArrayList<OcarinaButton>();
+//		// iterate through all the buttons to set their layout parameters
+//		for (int i = 0; i < Keys.values().length; i++)
+//		{
+//			OcarinaButton ocarinaButton = new OcarinaButton(Keys.values()[i], getSoundPlayer(), this);
+//			ocarinaButton.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+//			ocarinaButtons.add(ocarinaButton);
+//		}
+//		
+//		// create the layout
+//		TableLayout tableLayout = new TableLayout(this);
+//		tableLayout.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+//		
+//		// add ocarina buttons to a row and add the row to the parent table
+//		for (OcarinaButton ocarinaButton : ocarinaButtons)
+//		{
+//			TableRow tableRow = new TableRow(this);
+//			tableRow.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+//			tableRow.addView((View)ocarinaButton);
+//			tableLayout.addView((View)tableRow);
+//		}
+//		
+//		return tableLayout;
+//	}	
 }
